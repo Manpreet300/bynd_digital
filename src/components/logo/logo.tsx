5768cc46c46c9f490fb7c87b3602a96f42f1f3bf
hendricku <hendrickpaul.castro@lorma.edu>
2025-09-05 15:28:33 +0800

    a

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -8,7 +8,7 @@ export const Logo: React.FC<LogoProps> = ({ size, variant }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return (
     <LogoContainer>
-      {/* Assuming the logo is text-based. Replace with an <img /> if it's an image. */}
+    
       <LogoText className="LogoText">BYND DIGITAL</LogoText>
     </LogoContainer>
   );
